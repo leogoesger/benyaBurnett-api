e37716a97dc0e8422d710c6bd57b85de85f1816d
Leo Q <leoq91@gmail.com>
2018-08-07 19:53:24 -0700

    BB-26: api for crud

--- src/seeders/data/upcomingEvents.ts
+++ src/seeders/data/upcomingEvents.ts
@@ -1,5 +1,15 @@
 const upcomingEvents = [
     {
+        title:
+            "Focusing LIGHT on Sleep: Deep-Dive Investigation into the Sleep/Light Connection",
+        text:
+            "Numerous circadian conferences, wellness articles, and color tuning industry messaging points all mention the relationship of ambient light to sleep quality but fail to explain how and WHY the connection works; until now. With easy-to-understand medical research and neurobiological data detailing the physiological sleep process and downstream behavioral response, this session will provide attendees with the skill set needed to design, specify, and install lighting that offers more than color-changing lights in hope of providing occupants with better job performance and improved scholastic performance. Also, hear from two world-class sleep experts on the current state of circadian research and how their view of the lighting industry has changed.",
+        type: "upcoming-events",
+        imgUrl:
+            "https://www.lightshowwest.com/wp-content/uploads/Burnett_Deborah_speaker.jpg",
+        url: "https://www.lightshowwest.com/session/seminar-s3",
+    },
+    {
         title: "Lorem ipsum dolor sit amet1",
         text:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
