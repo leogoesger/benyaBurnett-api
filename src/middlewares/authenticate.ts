da8906fb6fe9e1b5fc86d944ebab4ae303f2766f
Leo Q <leoq91@gmail.com>
2018-10-17 17:36:49 -0700

    auto token bug

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -2,18 +2,19 @@ import { User } from '../models';
 import { Response } from 'express';
 
 const authenticate = (req: any, res: Response, next: any) => {
-    const token = req.header('Authorization');
-    User.findByToken(token)
-        .then(user => {
-            if (!user) {
-                res.status(401).send({ message: 'No user found!' });
-            }
-            req.body.user = user;
-            next();
-        })
-        .catch(e => {
-            res.status(401).send({ message: 'Authentication Failed!' });
-        });
+  const token = req.header('Authorization');
+  console.log('token authen midddle', token);
+  User.findByToken(token)
+    .then(user => {
+      if (!user) {
+        res.status(401).send({ message: 'No user found!' });
+      }
+      req.body.user = user;
+      next();
+    })
+    .catch(e => {
+      res.status(401).send({ message: 'Authentication Failed!' });
+    });
 };
 
 export default authenticate;
