f227271dd6bdded2f0b43556b6898ad98ea9a794
Leo Q <leoq91@gmail.com>
2018-10-26 16:25:16 -0700

    update mailing

--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -1,47 +1,47 @@
-import { Request, Response } from "express";
-import * as nodemailer from "nodemailer";
-import * as mg from "nodemailer-mailgun-transport";
-import { validateEmail } from "../utils/helpers";
+import { Request, Response } from 'express';
+import * as nodemailer from 'nodemailer';
+import * as mg from 'nodemailer-mailgun-transport';
+import { validateEmail } from '../utils/helpers';
 
 const auth = {
-    auth: {
-        api_key: process.env.MAILGUN_API_KEY,
-        domain: process.env.MAILGUN_DOMAIN,
-    },
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
 };
 
 interface IBody extends Request {
-    body: {
-        name: string;
-        email: string;
-        phone: string;
-        msg: string;
-    };
+  body: {
+    name: string;
+    email: string;
+    phone: string;
+    msg: string;
+  };
 }
 
 const nodeMailerMailgun = nodemailer.createTransport(mg(auth));
 
 const contactController = {
-    submit: (req: IBody, res: Response) => {
-        const { name, email, phone, msg } = req.body;
+  submit: (req: IBody, res: Response) => {
+    const { name, email, phone, msg } = req.body;
 
-        if (!name || !email || !phone || !msg || !validateEmail(email)) {
-            return res.status(400).send({ message: "Invalid Message!" });
-        }
+    if (!name || !email || !phone || !msg || !validateEmail(email)) {
+      return res.status(400).send({ message: 'Invalid Message!' });
+    }
 
-        const mailOptions = {
-            from: `${name} <${email}>`, // sender address
-            to: "leoq91@gmail.com", // list of receivers
-            subject: "Design Service Inc", // Subject line
-            text: "Phone: " + phone + "\n" + msg, // plain text body
-        };
-        nodeMailerMailgun.sendMail(mailOptions, error => {
-            if (error) {
-                res.status(400).send({ message: "Something Went Wrong" });
-            }
-        });
-        res.status(200).send({ message: "Message Sent!" });
-    },
+    const mailOptions = {
+      from: `${name} <${email}>`, // sender address
+      to: ['deborah@benyaburnett.com', 'leoq91@gmail.com'], // list of receivers
+      subject: 'Design Service Inc', // Subject line
+      text: 'Phone: ' + phone + '\n' + msg, // plain text body
+    };
+    nodeMailerMailgun.sendMail(mailOptions, error => {
+      if (error) {
+        res.status(400).send({ message: 'Something Went Wrong' });
+      }
+    });
+    res.status(200).send({ message: 'Message Sent!' });
+  },
 };
 
 export default contactController;
