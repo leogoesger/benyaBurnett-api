ee400b97d73d3f6063f37d34f516c029a399f26b
Leo Q <leoq91@gmail.com>
2019-02-04 20:28:14 -0800

    fix seeder

--- src/seeders/data/leedWell.ts
+++ src/seeders/data/leedWell.ts
@@ -9,13 +9,13 @@ const leedWell = [
     imgUrl: 'https://benya.nyc3.digitaloceanspaces.com/asid_image.png',
   },
   {
-    title: 'Lorem ipsum dolor sit amet1',
+    title: 'ASID project reserch and images',
     text:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     type: 'leed-well',
-    imgUrl:
-      'https://pbs.twimg.com/profile_images/846659478120366082/K-kZVvT8.jpg',
-    url: 'https://www.lipsum.com/',
+    imgUrl: 'https://benya.nyc3.digitaloceanspaces.com/asid/asid-report.png',
+    url:
+      'https://benya.nyc3.digitaloceanspaces.com/asid/ASID%20project%20reserch%20and%20images.pdf',
   },
 ];
 
--- src/seeders/data/upcomingEvents.ts
+++ src/seeders/data/upcomingEvents.ts
@@ -17,56 +17,6 @@ const upcomingEvents = [
       'https://www.lightshowwest.com/wp-content/uploads/Burnett_Deborah_speaker.jpg',
     url: 'https://www.lightshowwest.com/session/seminar-s3',
   },
-  {
-    title: 'Lorem ipsum dolor sit amet1',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    type: 'upcoming-events',
-    imgUrl:
-      'https://pbs.twimg.com/profile_images/846659478120366082/K-kZVvT8.jpg',
-    url: 'https://www.lipsum.com/',
-    date: '2018-08-07T17:38:49.590Z',
-  },
-  {
-    title: 'Lorem ipsum dolor sit amet2',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    type: 'upcoming-events',
-    imgUrl:
-      'https://pbs.twimg.com/profile_images/846659478120366082/K-kZVvT8.jpg',
-    url: 'https://www.lipsum.com/',
-    date: '2018-08-07T17:38:49.590Z',
-  },
-  {
-    title: 'Lorem ipsum dolor sit amet3',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    type: 'upcoming-events',
-    imgUrl:
-      'https://pbs.twimg.com/profile_images/846659478120366082/K-kZVvT8.jpg',
-    url: 'https://www.lipsum.com/',
-    date: '2018-08-07T17:38:49.590Z',
-  },
-  {
-    title: 'Lorem ipsum dolor sit amet4',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    type: 'upcoming-events',
-    imgUrl:
-      'https://pbs.twimg.com/profile_images/846659478120366082/K-kZVvT8.jpg',
-    url: 'https://www.lipsum.com/',
-    date: '2018-08-07T17:38:49.590Z',
-  },
-  {
-    title: 'Lorem ipsum dolor sit amet5',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    type: 'upcoming-events',
-    imgUrl:
-      'https://pbs.twimg.com/profile_images/846659478120366082/K-kZVvT8.jpg',
-    url: 'https://www.lipsum.com/',
-    date: '2018-08-07T17:38:49.590Z',
-  },
 ];
 
 export default upcomingEvents;
